f468019357f405117bb0c0f859af9a7c7573504d
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2024-12-09 14:47:07 -0600

    Removed the spawnpoint

--- MathForGamesDemo/src/Game/TurretActor.cs
+++ MathForGamesDemo/src/Game/TurretActor.cs
@@ -19,7 +19,7 @@ namespace MathForGamesDemo
         private Color _colorBullet = Color.Green;
         private Color _colorCollision = Color.Red;
 
-        //Vector2 spawnPoint = new Vector2(250, 250);
+        
         Rectangle rec;
         // Update the player transforms on start
         public override void Start()
@@ -60,21 +60,7 @@ namespace MathForGamesDemo
             // Shows the direction that you are facing.
             Raylib.DrawLineEx(Transform.GlobalPosition, Transform.GlobalPosition + Transform.Forward * 60, 10, Color.Red);
 
-            // If the player goes out of bounds respawn at spawnpoint
-            //if (Transform.LocalPosition.x > Raylib.GetScreenWidth()
-            //    || Transform.LocalPosition.y > Raylib.GetScreenHeight()
-            //    )
-            //{
-            //    // Spawn the vector back at spawnpoint
-            //    Transform.LocalPosition = spawnPoint;
-            //}
-
-            //if (Transform.LocalPosition.x <= 0
-            //    || Transform.LocalPosition.y <= 0
-            //    )
-            //{
-            //    Transform.LocalPosition = spawnPoint;
-            //}
+            
         }
         public override void OnCollision(Actor other)
         {
